a9b595866ca5398c28e90131ba87681b40f603bb
czhong888 <60477979+czhong888@users.noreply.github.com>
2021-04-10 01:47:41 -0400

    Add files via upload

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -1,67 +1,98 @@
 import sys
+import time
+import random
+import select
 import socket
 import struct
 
 
-# type|seqnum|length
-rudp_header_struct = struct.Struct('!III')
-rudp_header_size = rudp_header_struct.size
+rate = 0.9
+TIMEOUT = 1
+WINDOW_SIZE = 10
+RUDP_TYPE_ACK = 1
+RUDP_TYPE_PAYLOAD = 2
+RUDP_DATAGRAM_MAX_LEN = 1400
+# type|seq_num|payload_len|is_last_datagram
+rudp_header_struct = struct.Struct('!III?')
+RUDP_HEADER_LEN = rudp_header_struct.size
+RUDP_PAYLOAD_MAX_LEN = RUDP_DATAGRAM_MAX_LEN - RUDP_HEADER_LEN
 
-def rudp_receive_file(port, file):
-    seq_num = 0
+
+def rudp_receive_file(port):
+    data = b''
+    expect_seq_num = 0
+    all_received = False
+    recv_window = [None] * WINDOW_SIZE
+    last_seq_num = None
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('0.0.0.0', port))
-    with open(file, 'wb') as f:
-        while True:
-            data, client_addr = sock.recvfrom(1500)
-            data_len = len(data)
-            if data_len == 0:
-                break
-
-            if data_len < rudp_header_size:
+    sock.settimeout(TIMEOUT)
+    while True:
+        try:
+            datagram_data, addr = sock.recvfrom(RUDP_DATAGRAM_MAX_LEN)
+            datagram_data_len = len(datagram_data)
+            if datagram_data_len < RUDP_HEADER_LEN:
+                print('datagram too short, ignore', file=sys.stderr)
                 continue
 
-            t, data_seq_num, size = rudp_header_struct.unpack(data[:rudp_header_size])
-            print(t, data_seq_num, size, data_len)
-            if t != 0 or size < 0:
-                print('invalid data')
+            t, sn, l, is_last = rudp_header_struct.unpack(datagram_data[:RUDP_HEADER_LEN])
+            if t != RUDP_TYPE_PAYLOAD:
+                print('payload datagram expected, ignore', file=sys.stderr)
                 continue
 
-            if data_seq_num != seq_num and seq_num > 0:
-                # resend ack
-                sock.sendto(rudp_header_struct.pack(1, seq_num-1, 0), client_addr)
+            if sn > expect_seq_num + WINDOW_SIZE:
+                print('seq_num too great, ignore', file=sys.stderr)
                 continue
-            
-            # print('send_ack', client_addr)
+
+            print('receive {} datagram, {}, {}:'.format(sn, l, len(datagram_data[RUDP_HEADER_LEN:])), file=sys.stderr)
+            if is_last:
+                last_seq_num = sn
+
             # send ack
-            sock.sendto(rudp_header_struct.pack(1, seq_num, 0), client_addr)
-            # save data
-            f.write(data[rudp_header_size:])
-            seq_num = seq_num + 1
-
-            if (data_len - rudp_header_size) != 1400:
-                # last datagram
+            ack_datagram_header = rudp_header_struct.pack(RUDP_TYPE_ACK, sn, 0, False)
+            if random.random() < rate:
+                print('send ack datagram', sn, file=sys.stderr)
+                sock.sendto(ack_datagram_header, addr)
+            else:
+                print('discard ack datagram', sn, file=sys.stderr)
+            if sn >= expect_seq_num:
+                recv_window[sn - expect_seq_num] = datagram_data[RUDP_HEADER_LEN:]
+            
+            # check and update window
+            count = 0
+            while len(recv_window) > 0 and recv_window[0]:
+                count = count + 1
+                data = data + recv_window.pop(0)
+            
+            if count > 0:
+                recv_window.extend([None] * count)
+            expect_seq_num = expect_seq_num + count
+            if last_seq_num and expect_seq_num >= last_seq_num:
+                all_received = True
+        except socket.timeout:
+            if all_received:
                 break
-    sock.close()
-
+    
+    return data
+    
 
 
 def main():
     argc = len(sys.argv)
-    if argc != 3:
+    if argc != 2:
         sys.exit(1)
 
     try:
         port = int(sys.argv[1])
         if port < 1024 or port > 65535:
-            raise ValueError('bad receiver port')
+            raise ValueError('bad port')
     except ValueError:
         sys.exit(1)
 
-
-    return rudp_receive_file(port, sys.argv[2])
-
+    data = rudp_receive_file(port)
+    print(data.decode(), end='')
+    print('File received, exiting.', file=sys.stderr)
 
 if __name__ == '__main__':
     main()
-
--- project2/src/sender.py
+++ project2/src/sender.py
@@ -1,53 +1,133 @@
 import sys
 import time
+import random
+import select
 import socket
 import struct
 
-# type|seqnum|length
-rudp_header_struct = struct.Struct('!III')
-rudp_header_size = rudp_header_struct.size
 
-def rudp_send_file(receiver_ip, receiver_port, file):
+rate = 0.9
+TIMEOUT = 1
+WINDOW_SIZE = 10
+RUDP_TYPE_ACK = 1
+RUDP_TYPE_PAYLOAD = 2
+RUDP_DATAGRAM_MAX_LEN = 1400
+# type|seq_num|payload_len|is_last_datagram
+rudp_header_struct = struct.Struct('!III?')
+RUDP_HEADER_LEN = rudp_header_struct.size
+RUDP_PAYLOAD_MAX_LEN = RUDP_DATAGRAM_MAX_LEN - RUDP_HEADER_LEN
+
+
+def rudp_send_data(ip, port, data):
     seq_num = 0
+    data_len = len(data)
+    # split data into datagram payload
+    datagrams = []
+    datagram_count = data_len // RUDP_PAYLOAD_MAX_LEN
+    last_payload_len = data_len % RUDP_PAYLOAD_MAX_LEN
+    if last_payload_len != 0:
+        datagram_count = datagram_count + 1
+    else:
+        last_payload_len = RUDP_PAYLOAD_MAX_LEN
+    
+    print('about to send {} datagrams'.format(datagram_count), file=sys.stderr)
+
+    for i in range(datagram_count-1):
+        datagram_header = rudp_header_struct.pack(
+            RUDP_TYPE_PAYLOAD, seq_num, RUDP_PAYLOAD_MAX_LEN, False)
+        datagram_payload = data[(seq_num*RUDP_PAYLOAD_MAX_LEN):((seq_num+1)*RUDP_PAYLOAD_MAX_LEN)]
+        datagrams.append([seq_num, datagram_header + datagram_payload, None, False])
+        seq_num = seq_num + 1
+    # last datagram
+    datagram_header = rudp_header_struct.pack(
+        RUDP_TYPE_PAYLOAD,
+        seq_num,
+        last_payload_len,
+        True
+    )
+    datagram_payload = data[(datagram_count-1)*RUDP_PAYLOAD_MAX_LEN:]
+    datagrams.append([seq_num, datagram_header + datagram_payload, None, False])
+    
+    window_begin = 0
+    window_send = window_begin
+    window_end = window_begin + WINDOW_SIZE
+    if window_end >= datagram_count:
+        window_end = datagram_count
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(0.5)
-    with open(file, 'rb') as f:
-        while True:
-            chunk = f.read(1400)
-            chunk_len = len(chunk)
-            if chunk_len == 0:
-                break
-            header = rudp_header_struct.pack(0, seq_num, chunk_len)
-            while True:
-                sock.sendto(header+chunk, (receiver_ip, receiver_port))
-                # print('send data, len:', len(header+chunk))
-                try:
-                    ack_data, _ = sock.recvfrom(1500)
-                    ack_data_size = len(ack_data)
-                    if ack_data_size < rudp_header_size:
-                        # send again
-                        continue
+    sock.connect((ip, port))
+    while True:
+        # TODO: calc condition to stop
+        if window_begin >= window_end:
+            break
 
-                    t, ack_seq_num, size = rudp_header_struct.unpack(ack_data[0:rudp_header_size])
-                    # print('receive ack', ack_data_size, t, ack_seq_num, size)
-                    if t != 1 or ack_seq_num != seq_num or size != 0:
-                        # send again
-                        continue
+        # send datagrams
+        for i in range(window_send, window_end):
+            if random.random() < rate:
+                print('send datagram:', datagrams[i][0], file=sys.stderr)
+                sock.send(datagrams[i][1])
+            else:
+                print('discard {} datagram'.format(datagrams[i][0]), file=sys.stderr)
+            datagrams[i][2] = time.time()
+            window_send = window_send + 1
+        
+        # calculate timeout
+        timeout = 1.0
+        now = time.time()
+        for i in range(window_begin, window_send):
+            t = now - datagrams[i][2]
+            if t < timeout:
+                timeout = t
+        
+        rlist, _, _ = select.select([sock], [], [], timeout)
+        if sock in rlist:
+            datagram = sock.recv(RUDP_DATAGRAM_MAX_LEN)
+            datagram_len = len(datagram)
+            if datagram_len < RUDP_HEADER_LEN:
+                print('datagram too short', file=sys.stderr)
+            else:
+                t, sn, _, _ = rudp_header_struct.unpack(datagram[:RUDP_HEADER_LEN])
+                if t != RUDP_TYPE_ACK:
+                    print('ack datagram expected', file=sys.stderr)
+                else:
+                    print('receive ack packet for seq num:', sn, file=sys.stderr)
+                    for i in range(window_begin, window_send):
+                        if datagrams[i][0] == sn:
+                            datagrams[i][3] = True
+                            break
 
-                    seq_num  = seq_num + 1
-                    break
-                except socket.timeout:
-                    # send again
-                    print('timeout, resend data')
-                    continue
+        # check timeout, send timeout datagrams
+        update_count = 0
+        now = time.time()
+        last_ack = True
+        for i in range(window_begin, window_send):
+            if last_ack and datagrams[i][3]:
+                update_count = update_count + 1
+            else:
+                last_ack = False
+            
+            if now - datagrams[i][2] > TIMEOUT:
+                if random.random() < rate:
+                    sock.send(datagrams[i][1])
+                    print('re-send datagram:', datagrams[i][0], file=sys.stderr)
+                else:
+                    print('re-discard {} datagram'.format(datagrams[i][0]), file=sys.stderr)
+                datagrams[i][2] = now
+        
+        # update window
+        window_begin = window_begin + update_count
+        window_end = window_end + update_count
+        if window_end >= datagram_count:
+            window_end = datagram_count
+    
     sock.close()
+
 
 
 def main():
     argc = len(sys.argv)
-    if argc != 4:
+    if argc != 3:
         sys.exit(1)
-
+    
     receiver_ip = sys.argv[1]
     try:
         receiver_port = int(sys.argv[2])
@@ -55,9 +135,15 @@ def main():
             raise ValueError('bad receiver port')
     except ValueError:
         sys.exit(1)
-    
-    # print(type(data_to_send), len(data_to_send))
-    return rudp_send_file(receiver_ip, receiver_port, sys.argv[3])
+
+    # data = sys.stdin.read()
+    beg_time = time.time()
+    data = sys.stdin.read()
+    rudp_send_data(receiver_ip, receiver_port, data.encode())
+    data_len = len(data)
+    time_used = time.time() - beg_time
+    print('Sent {} bytes in {} seconds: {} kB/s'.format(
+        data_len, int(time_used), int(data_len/1024/time_used)), file=sys.stderr)
 
 
 
